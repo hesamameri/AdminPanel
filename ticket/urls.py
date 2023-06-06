7ef749504ce5ff5ea298934c18f8153a1948a6ba
hesamameri <hesam7991ameri@gmail.com>
2023-06-06 09:05:41 +0330

    new setup changes

--- ticket/urls.py
+++ ticket/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('', views.login_view, name='login'),
+    path('confirmTicket', views.confirm_ticket_view, name='confirmTicket'),
     
    
 ]
--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def confirm_ticket_view(request):
+    return render(request,'./Ticket/confirmTicket.html',{})
+def confirm_ticket_view(request):
+    pass
+def confirm_ticket_view(request):
+    pass
+def confirm_ticket_view(request):
+    pass
+def confirm_ticket_view(request):
+    pass
+def confirm_ticket_view(request):
+    pass
+def confirm_ticket_view(request):
+    pass
