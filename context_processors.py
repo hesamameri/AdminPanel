dc9c97d5d88a3802617bb0239bc961856a285562
hesamameri <hesam7991ameri@gmail.com>
2023-07-03 07:41:43 +0330

    template processors

--- context_processors.py
+++ context_processors.py
@@ -1,3 +1,14 @@
 
 def common_data(request):
     pass
+from Administrator.models import User
+
+def user_data(request):
+
+    user = request.session['user_id']
+    user = User.objects.filter(user_id = user)[0]
+    
+    context = {
+        'user': user,
+    }
+    return context
--- panel/settings.py
+++ panel/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Company_panel.context_processors.common_data',
+                'context_processors.user_data',
             ],
             'libraries':{
             'converter_tags': 'ticket.templatetags.converter_tags',
