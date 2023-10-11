7368d813f221b9ad7b2eceed9fb5f31435a28c5d
amirhosseinahmadi1379 <130206291+amirhosseinahmadi1379@users.noreply.github.com>
2023-10-11 12:32:49 +0330

    fixing the problem that show nothing for Customers

--- customer/views.py
+++ customer/views.py
@@ -81,7 +81,7 @@ def customer_index(request):
         # print(tickets)
         pre_factors = PreFactor.objects.filter(buyer_id__in=customer_ids)
         # print(pre_factors)
-        factors = Factor.objects.filter(buyer_id__in=customer_ids,)
+        factors = Factor.objects.filter(buyer_id__in=customer_ids)
         # print(factors)
         banks = ObjItem.objects.filter(obj_item_id__gte=999003010, obj_item_id__lte=999003019)        # Set the number of records to display per page
 
@@ -313,17 +313,14 @@ def customer_index_all(request,shop_id = None):
         if shop_id is None: 
             # Get distinct obj_item_ids with maximum values for each field
             # distinct_records = CustomerSva.objects.all()
-            customers = CustomerSva.objects.filter(obj_item_id__gt = 1030200027).order_by('-obj_item_id')
+            customers = CustomerSva.objects.filter(obj_item_id__gt = 1030200027).order_by('obj_item_id')
             page = customers
             customer_ids = customers.values_list('obj_item_id', flat=True)
             # Retrieve related objects based on customer_ids
             obj_payments = ObjPayment.objects.filter(obj_item_id__in=customer_ids)
-            print(obj_payments)
             tickets = Ticket.objects.filter(obj_source_id__in=customer_ids) # assuming obj_source_id is the relevant field in Ticket model
-            print(tickets)
             pre_factors = PreFactor.objects.filter(buyer_id__in=customer_ids)
-            print(pre_factors)
-            factors = Factor.objects.filter(buyer_id__in=customer_ids, reg_status='CONFIRM')
+            factors = Factor.objects.filter(buyer_id__in=customer_ids)
             # customer_ids = distinct_records.values_list('obj_item_id', flat=True)
             # obj_payments = ObjPayment.objects.filter(obj_item_id__in=customer_ids)
             # tickets = Ticket.objects.filter(obj_source_id__in=customer_ids) # assuming obj_source_id is the relevant field in Ticket model
@@ -363,7 +360,7 @@ def customer_index_all(request,shop_id = None):
         else:
             # Get distinct obj_item_ids with maximum values for each field
             # distinct_records = CustomerSva.objects.all()
-            customers = CustomerSva.objects.filter(obj_item_id__gt = 1030200027,reagent = shop_id).order_by('-obj_item_id')
+            customers = CustomerSva.objects.filter(obj_item_id__gt = 1030200027).order_by('obj_item_id')
             page = customers
             customer_ids = customers.values_list('obj_item_id', flat=True)
             # Retrieve related objects based on customer_ids
@@ -372,7 +369,6 @@ def customer_index_all(request,shop_id = None):
             tickets = Ticket.objects.filter(obj_source_id__in=customer_ids) # assuming obj_source_id is the relevant field in Ticket model
             print(tickets)
             pre_factors = PreFactor.objects.filter(buyer_id__in=customer_ids)
-            print(pre_factors)
             factors = Factor.objects.filter(buyer_id__in=customer_ids, reg_status='CONFIRM')
             # customer_ids = distinct_records.values_list('obj_item_id', flat=True)
             # obj_payments = ObjPayment.objects.filter(obj_item_id__in=customer_ids)
